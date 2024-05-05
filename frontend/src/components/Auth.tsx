836686d76c450ae5053254faa109afd7fdb5e4aa
Shubham Gupta <shubham272101@gmail.com>
2024-05-06 03:15:02 +0530

    update files

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,33 +1,51 @@
 import { ChangeEvent, useState } from "react";
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { SignupInput } from "@shubhamgupta2701/medium-common";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export  const Auth = ({type}:{type:"signup" | "signin"})=>{
+    const navigate = useNavigate();
     const [postInputs, setPostInputs] = useState<SignupInput>({
         name : "",
         email : "",
         password : "" 
     });  
 
+
+    async function sendRequest(){
+        try{
+            const response = await axios.post(`${BACKEND_URL}/api/v1/user/${type === "signin" ? "signin" : "signup"}`,postInputs)
+            const jwt = response.data;
+            localStorage.setItem("token",jwt);
+            navigate("/blogs");
+        }catch(e){
+            console.log(e);
+        }
+    }
+
     return (
         <div className="flex justify-center h-screen flex-col">
             <div className="flex justify-center">
                 <div>
-                    <div className="text-4xl font-extrabold">
-                        Create a Account
+                    <div className="text-4xl font-extrabold px-10">
+                        {type === "signin" ? "Welcome back" : "Create an Account"}
                     </div>
                     <div className="text-center text-slate-400">
-                        Already have an accout?
-                        <Link className="pl-2 underline" to={"/signin"}>Login</Link> 
+                        {type === "signin" ? "Don't have an Account" : "Already have an Account"}
+                        <Link className="pl-2 underline" to={type==="signin"?"/signup":"/signin"}>{type === "signin" ? "Sign up" : "Login"}</Link>
                     </div>
                     <div className="mt-6">
-                    <LabelledInput label = "Name" placeholder="Enter Your Name" onChange={(e)=>{
+                    {type === "signup" ? <LabelledInput label = "Name" type = "text" placeholder="Enter Your Name" onChange={(e)=>{
+                    setPostInputs(c=>({...c,name:e.target.value}))}} /> : null}
+                    <LabelledInput label = "Email" type = "email" placeholder="something@gmail.com" onChange={(e)=>{
                     setPostInputs(c=>({...c,name:e.target.value}))}} />
-                    <LabelledInput label = "Email" placeholder="something@gmail.com" onChange={(e)=>{
-                    setPostInputs(c=>({...c,name:e.target.value}))}} />
-                    <LabelledInput label = "Password" placeholder="*********" onChange={(e)=>{
+                    <LabelledInput label = "Password" type = "password" placeholder="*********" onChange={(e)=>{
                     setPostInputs(c=>({...c,name:e.target.value}))}} />
                     </div> 
+                    <div>
+                    <button onClick={sendRequest} type="submit" className="mt-6 flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{type === "signin" ? "Log In" : "Sign up "}</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -37,15 +55,16 @@ export  const Auth = ({type}:{type:"signup" | "signin"})=>{
 interface LabelledInputType {
     label : string;
     placeholder : string;
+    type : string;
     onChange : (e:ChangeEvent<HTMLInputElement>) => void;
 }
 
-function LabelledInput({ label, placeholder, onChange} : LabelledInputType){
+function LabelledInput({ label, placeholder, onChange, type} : LabelledInputType){
     return <div>
     <div>
-        <label className="block text-sm font-medium leading-6 text-gray-900">{label}</label>
+        <label className="block text-sm font-bold leading-6 text-gray-900">{label}</label>
             <div className="mt-2 mb-2">
-                <input onChange ={onChange}id="text" name="first_name" className="block w-full pl-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder={placeholder} required />
+                <input onChange ={onChange} type={type||"text"} id="text" name={label}  className="block w-full pl-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder={placeholder} required />
             </div>
         </div>
     </div>
