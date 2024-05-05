97f25f9be638fed20c84552fc0545d417b6a7dd9
Shubham Gupta <shubham272101@gmail.com>
2024-05-06 03:13:57 +0530

    update version-1 of signin and signup components

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,8 +3,8 @@ import { Quote } from "../components/Quote"
 
 export const Signin = ()=>{
     return <div>
-        <div className="grid grid-cols-2">
-            <div className="invisible lg:visible">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div className="hidden lg:block">
                 <Quote />
             </div>  
             <div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,11 +3,11 @@ import { Quote } from "../components/Quote"
 
 export const Signup = ()=>{
     return <div>
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
             <div>
                 <Auth type={"signup"} />
             </div>
-            <div className="invisible lg:visible">
+            <div className="hidden lg:block">
                 <Quote />
             </div>    
         </div>
