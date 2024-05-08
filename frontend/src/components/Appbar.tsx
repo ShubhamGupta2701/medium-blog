afd8f22833bd6955aade0d04255e9d8e43c8b16c
Shubham Gupta <shubham272101@gmail.com>
2024-05-08 18:03:39 +0530

    udate appbar and blog component

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -2,7 +2,7 @@ import {Avatar} from "./BlogCard"
 
 export const Appbar = () => {
     return <div className="border-b flex justify-between px-10 py-2 ">
-        <div className="text-lg font-semibold font-mono">
+        <div className="text-lg font-semibold font-mono flex flex-col justify-center">
             Medium Bloging
         </div>
         <div>
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -6,9 +6,9 @@ export const Blogs = () =>{
         <Appbar/>
         <div className="flex justify-center">
             <div className="max-w-xl">
-                <BlogCard authorName="Shubham gupta" title = "My first Blog, create during the development of the medium web app !!!" content = "this is my first blog and also this for testing purpose during the production period of the project of my medium bloging webApp" publishDate="08-April-2024" />
-                <BlogCard authorName="Riya Goel" title = "How to learn by doing nothing, specialized approach !!!!!" content = "Hi in this blog , i am going to tell you how to learn and complete all the task without doing nothing, you just have to tell some other person to do it for you" publishDate="07-April-2024" />
-                <BlogCard authorName="Shubham gupta" title = "Latest Tech Stack, Which helps you to get job faster and make you more efficient !!!" content = "Latest Trends in IT are : Next JS , Hono - cloudflare workers, PostgreSQL,Flutter, Firebase, Node JS, Express, React, BlockChain, Rust - for faster compilations. There are many more technologies which may help you to land a job but these are the most common one which are in demand in India" publishDate="10-April-2024" />
+                <BlogCard authorName={"Shubham gupta"} title = {"My first Blog, create during the development of the medium web app !!!"} content = {"this is my first blog and also this for testing purpose during the production period of the project of my medium bloging webApp"} publishDate={"08-April-2024"} />
+                <BlogCard authorName={"Riya Goel"} title = {"How to learn by doing nothing, specialized approach !!!!!"} content = {"Hi in this blog , i am going to tell you how to learn and complete all the task without doing nothing, you just have to tell some other person to do it for you"} publishDate={"07-April-2024"} />
+                <BlogCard authorName={"Shubham gupta"} title = {"Latest Tech Stack, Which helps you to get job faster and make you more efficient !!!"} content = {"Latest Trends in IT are : Next JS , Hono - cloudflare workers, PostgreSQL,Flutter, Firebase, Node JS, Express, React, BlockChain, Rust - for faster compilations. There are many more technologies which may help you to land a job but these are the most common one which are in demand in India"}publishDate={"10-April-2024"} />
             </div>
         </div>
     </div>
