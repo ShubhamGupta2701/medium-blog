ed59d20de6bafc1e6a8bbfbbdb2cc5b6ca620279
Shubham Gupta <shubham272101@gmail.com>
2024-05-08 15:21:32 +0530

    create blogs component

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,5 +1,29 @@
-export const BlogCard = () =>{
+
+interface BlogcardProps{
+    authorName : string;
+    title : string;
+    content : string;
+    publishDate : string;
+}
+
+export const BlogCard = ({authorName,title,content,publishDate}:BlogcardProps) =>{
     return <div>
-        Blog Card
+        <div>
+            <div>
+                {authorName}
+            </div>
+            <div>
+                {publishDate}
+            </div>
+        </div>
+        <div>
+            {title}
+        </div>
+        <div>
+            {content.slice(0,100) + "...."}
+        </div>
+        <div>
+            {`${Math.ceil(content.length/100)} minute's Read`}
+        </div>
     </div>
 }
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,5 +1,7 @@
+import { BlogCard } from "../components/BlogCard"
+
 export const Blogs = () =>{
     return <div>
-        All Blogs
+        <BlogCard authorName="Shubham gupta" title = "My first BLog" content = "this is my first blog and also this for testing purpose during the production period of the project of my medium bloging webApp" publishDate="08-April-2024" />
     </div>
 }
