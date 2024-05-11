ea53258cd849f7e24481ca8f88499ced68a91272
Shubham Gupta <shubham272101@gmail.com>
2024-05-11 12:05:48 +0530

    add UniqueBlog component

--- frontend/src/components/UniqueBlog.tsx
+++ frontend/src/components/UniqueBlog.tsx
@@ -1,8 +1,20 @@
 import { Appbar } from "./Appbar"
+import { Blog } from "../hooks"
 
-export const UniqueBlog = () => {
-    return <div>
+export const UniqueBlog = ({blog} : {blog:Blog}) => {
+    return <div className="font-serif">
         <Appbar />
-        hi there
+        <div className="grid grid-cols-12 px-10 w-full pt-8">
+            <div className="col-span-8">
+                <div className="text-3xl font-extrabold">
+                    {blog.title}
+                </div>
+                <div className="text-xl mt-4">
+                    {blog.content}
+                </div>
+            </div>
+            <div className="col-span-4">
+            </div>
+        </div>
     </div>
 }
--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -13,6 +13,6 @@ export const Blog = ()=>{
         </div>
     }
     return <div>
-        <UniqueBlog />
+        <UniqueBlog blog={blog}/>
     </div>
 }
