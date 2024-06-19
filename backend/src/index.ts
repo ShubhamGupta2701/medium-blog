cbf427880bf470a2685ea4168dcb2c8a28f55d37
Shubham Gupta <shubham272101@gmail.com>
2024-06-19 23:52:35 +0530

    add comments in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,11 @@ import {cors} from 'hono/cors'
 
 export const app = new Hono<{
   Bindings: {
-      DATABASE_URL: string;
+      DATABASE_URL: string; // prisma vala url hit hoga isme
       JWT_SECRET: string;
   }
 }>();
-app.use('/*',cors());
+app.use('/*',cors()); // middleware - ye vo predefined functions hoty h jo hmesha run hoge allover
 app.route('/api/v1/user', userRouter)
 app.route('/api/v1/blog', blogRouter)
 
